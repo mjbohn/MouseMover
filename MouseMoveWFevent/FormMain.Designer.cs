81c2169d3643e7dc132105dd0f16db562408985d
Michael Bohn <mjbohn@gmail.com>
2025-07-04 11:11:19 +0200

    inital commit

---
+++ MouseMoveWFevent/FormMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MouseMoveWFevent
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStatus = new Label();
+            lblLastMove = new Label();
+            numericUpDownwaitTime = new NumericUpDown();
+            lblWaitTime = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownwaitTime).BeginInit();
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(23, 15);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(38, 15);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "status";
+            // 
+            // lblLastMove
+            // 
+            lblLastMove.AutoSize = true;
+            lblLastMove.Location = new Point(23, 30);
+            lblLastMove.Name = "lblLastMove";
+            lblLastMove.Size = new Size(16, 15);
+            lblLastMove.TabIndex = 1;
+            lblLastMove.Text = "...";
+            // 
+            // numericUpDownwaitTime
+            // 
+            numericUpDownwaitTime.Location = new Point(87, 51);
+            numericUpDownwaitTime.Maximum = new decimal(new int[] { 3600, 0, 0, 0 });
+            numericUpDownwaitTime.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDownwaitTime.Name = "numericUpDownwaitTime";
+            numericUpDownwaitTime.Size = new Size(52, 23);
+            numericUpDownwaitTime.TabIndex = 2;
+            numericUpDownwaitTime.Value = new decimal(new int[] { 180, 0, 0, 0 });
+            numericUpDownwaitTime.ValueChanged += numericUpDownwaitTime_ValueChanged;
+            // 
+            // lblWaitTime
+            // 
+            lblWaitTime.AutoSize = true;
+            lblWaitTime.Location = new Point(23, 53);
+            lblWaitTime.Name = "lblWaitTime";
+            lblWaitTime.Size = new Size(49, 15);
+            lblWaitTime.TabIndex = 3;
+            lblWaitTime.Text = "wait sec";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(185, 110);
+            Controls.Add(lblWaitTime);
+            Controls.Add(numericUpDownwaitTime);
+            Controls.Add(lblLastMove);
+            Controls.Add(lblStatus);
+            MaximizeBox = false;
+            Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MouseMove";
+            FormClosed += FormMain_FormClosed;
+            ((System.ComponentModel.ISupportInitialize)numericUpDownwaitTime).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private Label lblLastMove;
+        private NumericUpDown numericUpDownwaitTime;
+        private Label lblWaitTime;
+    }
+}
---
+++ MouseMoveWFevent/FormMain.cs
@@ -0,0 +1,132 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Timer = System.Windows.Forms.Timer;
+
+
+namespace MouseMoveWFevent;
+
+public partial class FormMain : Form
+{
+    private Timer inactivityTimer;
+    private IntPtr hookId = IntPtr.Zero;
+    private NativeMethods.LowLevelMouseProc mouseProc;
+
+    private string _status_watch = "watching mouse moves...";
+    private string _status_sim = "simulating mouse move...";
+    private string _status_move_detected = "mouse move detected: ";
+
+    public FormMain()
+    {
+        InitializeComponent();
+
+        inactivityTimer = new Timer();
+        inactivityTimer.Interval = (int)numericUpDownwaitTime.Value * 1000;
+        inactivityTimer.Tick += (s, e) => SimulateMouseMove();
+        inactivityTimer.Start();
+
+        mouseProc = HookCallback;
+        hookId = NativeMethods.SetHook(mouseProc);
+
+        lblStatus.Text = _status_watch;
+    }
+
+    private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0)
+        {
+            // Mouse move detected => reset timer
+
+            if (inactivityTimer.Enabled)
+            {
+                inactivityTimer.Stop();
+                inactivityTimer.Start();
+            }
+
+            lblStatus.Invoke((MethodInvoker)(() =>
+            {
+                lblLastMove.Text = $"{_status_move_detected} {DateTime.Now:T}";
+
+            }));
+
+
+        }
+        return NativeMethods.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+    }
+
+    private void SimulateMouseMove()
+    {
+        lblStatus.Text = $"{_status_sim} {DateTime.Now:T}";
+
+        NativeMethods.INPUT[] input = new NativeMethods.INPUT[2];
+        input[0] = new NativeMethods.INPUT
+        {
+            type = NativeMethods.INPUT_MOUSE,
+            mi = new NativeMethods.MOUSEINPUT { dx = 10, dy = 10, dwFlags = NativeMethods.MOUSEEVENTF_MOVE }
+        };
+        input[1] = new NativeMethods.INPUT
+        {
+            type = NativeMethods.INPUT_MOUSE,
+            mi = new NativeMethods.MOUSEINPUT { dx = -10, dy = -10, dwFlags = NativeMethods.MOUSEEVENTF_MOVE }
+        };
+
+        NativeMethods.SendInput((uint)input.Length, input, Marshal.SizeOf(typeof(NativeMethods.INPUT)));
+    }
+
+    private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        NativeMethods.UnhookWindowsHookEx(hookId);
+    }
+
+    private void numericUpDownwaitTime_ValueChanged(object sender, EventArgs e)
+    {
+        inactivityTimer.Interval = (int)numericUpDownwaitTime.Value * 1000;
+    }
+}
+
+internal static class NativeMethods
+{
+    public const int WH_MOUSE_LL = 14;
+    public const int INPUT_MOUSE = 0;
+    public const int MOUSEEVENTF_MOVE = 0x0001;
+
+    public delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MOUSEINPUT
+    {
+        public int dx, dy, mouseData, dwFlags, time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct INPUT
+    {
+        public int type;
+        public MOUSEINPUT mi;
+    }
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll")]
+    public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    public static IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule!)
+        {
+            return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+}
+
